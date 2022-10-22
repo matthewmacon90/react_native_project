66f54792261c3d0d657a19fdc4698f4db2782210
Matthew Macon <matthewmacon90@gmail.com>
2022-10-22 13:05:31 -0400

    Workshop 3

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from 'expo-image-picker';
 import {baseUrl} from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
 
 const LoginTab = ({navigation}) => {
     const [username, setUsername] = useState('');
@@ -149,7 +151,31 @@ const RegisterTab = () => {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                setImageUrl(capturedImage.uri);
+                processImage(capturedImage.uri);
+            }
+        }
+    };
+
+    const processImage = async(imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri, [{resize:{width: 400}}], {format: ImageManipulator.SaveFormat.PNG}
+        );
+        console.log('Processed Imaged:', processedImage);
+        setImageUrl(processedImage.uri);
+        MediaLibrary.saveToLibraryAsync(processedImage.uri);
+    };
+
+    const getImageFromGallery = async() => {
+        const mediaLibraryPermissions = await ImagePicker.requestCameraPermissionsAsync();
+        
+        if (mediaLibraryPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1,1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                processImage(capturedImage.uri);
             }
         }
     };
@@ -164,6 +190,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title='Camera' onPress={getImageFromCamera} />
+                    <Button title='Gallery' onPress={getImageFromGallery} />
                 </View>
                 <Input 
                     placeholder="Username"
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -239,19 +239,44 @@ const Main = () => {
         dispatch(fetchComments());
     }, [dispatch]);
 
-    useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
+    const showNetInfo = async() => {
+        const connectionInfo = await NetInfo.fetch();
             Platform.OS === 'ios'
-                ? Alert.alert(
-                      'Initial Network Connectivity Type:',
-                      connectionInfo.type
-                  )
-                : ToastAndroid.show(
-                      'Initial Network Connectivity Type: ' +
-                          connectionInfo.type,
-                      ToastAndroid.LONG
-                  );
-        });
+            ? Alert.alert(
+                'Initial Network Connectivity Type:',
+                connectionInfo.type
+            )
+            : ToastAndroid.show(
+                'Initial Network Connectivity Type: ' +
+                    connectionInfo.type,
+                ToastAndroid.LONG
+            );
+
+        // const NetInfo = await fetch((connectionInfo) => {
+        //     Platform.OS === 'ios'
+        //         ? Alert.alert(
+        //               'Initial Network Connectivity Type:',
+        //               connectionInfo.type
+        //           )
+        //         : ToastAndroid.show(
+        //               'Initial Network Connectivity Type: ' +
+        //                   connectionInfo.type,
+        //               ToastAndroid.LONG
+        //           );
+        // });
+
+        // NetInfo.fetch().then((connectionInfo) => {
+        //     Platform.OS === 'ios'
+        //         ? Alert.alert(
+        //               'Initial Network Connectivity Type:',
+        //               connectionInfo.type
+        //           )
+        //         : ToastAndroid.show(
+        //               'Initial Network Connectivity Type: ' +
+        //                   connectionInfo.type,
+        //               ToastAndroid.LONG
+        //           );
+        // });
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
@@ -260,6 +285,11 @@ const Main = () => {
         );
 
         return unsubscribeNetInfo;
+    };
+
+
+    useEffect(() => {
+        showNetInfo();
     }, []);
 
     const handleConnectivityChange = (connectionInfo) => {
